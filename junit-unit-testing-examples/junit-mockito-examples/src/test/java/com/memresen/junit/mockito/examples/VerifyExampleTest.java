af7c617d7ea744b3ee30df8dd8e6e2826aff9026
Emre Sen <maemresen@yazilim.vip>
2023-08-27 16:59:57 +0300

    mockito examples updated

--- junit-unit-testing-examples/junit-mockito-examples/src/test/java/com/memresen/junit/mockito/examples/VerifyExampleTest.java
+++ junit-unit-testing-examples/junit-mockito-examples/src/test/java/com/memresen/junit/mockito/examples/VerifyExampleTest.java
@@ -2,10 +2,8 @@ package com.memresen.junit.mockito.examples;
 
 import com.maemresen.junit.mockito.examples.service.PersonService;
 import com.maemresen.junit.mockito.examples.service.domain.Person;
-import com.maemresen.junit.mockito.examples.service.exception.BusinessException;
 import com.maemresen.junit.mockito.examples.service.impl.PersonServiceImpl;
 import com.maemresen.junit.mockito.examples.service.repository.PersonRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,32 +13,42 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.description;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(MockitoExtension.class) // Indicate that Mockito should process this class for annotations
 class VerifyExampleTest {
 
+    // The service we want to test
     PersonService personService;
 
+    // Mocking the repository to isolate the service for unit testing
     @Mock
     PersonRepository personRepository;
 
     @BeforeEach
     void init() {
+        // Initializing the service with the mocked repository before each test
         personService = new PersonServiceImpl(personRepository);
     }
 
     @Test
     @DisplayName("Given existing person name and above age 18, when isAdultContentAllowed, then true")
-    void givenExistingPersonNameAndAboveAge18_whenIsAdultContentAllowed_thenTrue() throws BusinessException {
+    void givenExistingPersonNameAndAboveAge18_whenIsAdultContentAllowed_thenTrue() {
+        // Sample data for the test
         final var personName = "Mock";
         final var mockPerson = new Person("Mock", 19);
-        when(personRepository.findByName(personName)).thenReturn(Optional.of(mockPerson));
-        verify(personRepository, description("Person should be searched by name")).findByName(personName);
-        Assertions.assertTrue(personService.isAdultContentAllowed(personName), "Adult content should be allowed");
-    }
 
+        // Mocking the repository response for the test scenario
+        when(personRepository.findByName(personName)).thenReturn(Optional.of(mockPerson));
+
+        assertAll(
+                // Assert that the service behaves correctly given the mocked data
+                () -> assertTrue(personService.isAdultContentAllowed(personName), "Adult content should be allowed"),
+                // Verifying that the repository's findByName method was called with the expected parameter
+                () -> verify(personRepository).findByName(personName)
+        );
+    }
 }
