6d9eb8f2e07d8ccee47011da7011d48f26f06ce1
Emre Sen <maemresen@yazilim.vip>
2023-08-27 19:17:13 +0300

    spring boot IT tests added

---
+++ junit-integration-testing-examples/junit-spring-boot-it-examples/spring-boot-basic-it-with-database/src/main/java/com/maemresen/spring/boot/basic/it/with/database/JunitSpringBootItExampleApplication.java
@@ -0,0 +1,11 @@
+package com.maemresen.spring.boot.basic.it.with.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitSpringBootItExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JunitSpringBootItExampleApplication.class, args);
+    }
+}
